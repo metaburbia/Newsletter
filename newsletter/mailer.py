16cd628331ffcffea99f93561af0d2dac28955f7
David Jones <david@metaburbia.com>
2011-07-03 11:36:12 +0100

    mailer changed - MIME

--- newsletter/mailer.py
+++ newsletter/mailer.py
@@ -1,5 +1,12 @@
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEImage import MIMEImage
 import smtplib
 import mailcon
+# Define these once; use them twice!
+strFrom = 'from@example.com'
+strTo = 'to@example.com'
+
 
 
 def sendmail(filename):
@@ -11,7 +18,18 @@ def sendmail(filename):
     server.starttls()
     server.ehlo()
     server.login(mailcon.mailuser,mailcon.mailpassword)
-    FROM = mailcon.mailfrom
+    
+    # Create the root message and fill in the from, to, and subject headers
+    msgRoot = MIMEMultipart('related')
+    msgRoot['Subject'] = 'Holiday newsletter'
+    msgRoot['From'] =  mailcon.mailfrom
+    msgRoot['To'] = mailcon.mailto
+    msgRoot.preamble = 'This is a multi-part message in MIME format.'
+    msgAlternative = MIMEMultipart('alternative')
+    msgRoot.attach(msgAlternative)
+    msgText = MIMEText(msg)
+    msgAlternative.attach(msgText)
+    '''FROM = mailcon.mailfrom
     TO = [mailcon.mailto] # must be a list
     SUBJECT = "Holiday newsletter"
     TEXT = msg
@@ -22,6 +40,7 @@ def sendmail(filename):
     %s
     """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
     # Send the mail
-    server.sendmail(FROM, TO, message)
+    '''
+    server.sendmail(mailcon.mailfrom, mailcon.mailto, msgRoot.as_string())
     server.quit()
     server.close()
